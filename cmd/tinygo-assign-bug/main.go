196295adfdb7b8813aa481ba37870686fd4049d3
Randy Reddig <randy.reddig@fastly.com>
2023-10-17 12:01:16 -0700

    cmd/tinygo-assign-bug: small program demonstrating assignment evaluation order difference between Go and TinyGo

---
+++ cmd/tinygo-assign-bug/main.go
@@ -0,0 +1,31 @@
+package main
+
+/*
+This program demonstrates a difference in behavior between Go and TinyGo.
+In Go, the multiple assignment is evaluated after the call to toUpper.
+In TinyGo, the assignment happens prior to the call to toUpper.
+
+See: https://go.dev/ref/spec#Assignment_statements
+
+$ go run ./cmd/tinygo-assign-bug
+HELLO HELLO <nil>
+
+$ tinygo run ./cmd/tinygo-assign-bug
+HELLO hello <nil>
+*/
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	s1 := "hello"
+	s2, err := s1, toUpper(&s1)
+	fmt.Println(s1, s2, err)
+}
+
+func toUpper(s *string) error {
+	*s = strings.ToUpper(*s)
+	return nil
+}
