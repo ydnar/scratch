344c00ca29a53534df2c712992a72412f96cf76e
Randy Reddig <randy.reddig@fastly.com>
2024-01-06 14:20:36 +1300

    cmd/wasip2-test: PoC for native WASI Preview 2 support in TinyGo
    https://github.com/dgryski/tinygo/tree/dgryski/wasi-preview-2

---
+++ cmd/wasip2-test/main.go
@@ -0,0 +1,8 @@
+package main
+
+// This requires TinyGo with WASI Preview 2 support
+// https://github.com/dgryski/tinygo/tree/dgryski/wasi-preview-2
+
+func main() {
+	print("hello world\n")
+}
---
+++ wasip2run.sh
@@ -0,0 +1,15 @@
+# This requires TinyGo with WASI Preview 2 support
+# https://github.com/dgryski/tinygo/tree/dgryski/wasi-preview-2
+
+tinygo build -x -target=wasip2 -o main.wasm ./cmd/wasip2-test
+wasm-tools component embed -w wasi:cli/command $(tinygo env TINYGOROOT)/lib/wasm/wasip2/wit/ main.wasm -o embedded.wasm
+wasm-tools component new embedded.wasm -o component.wasm
+wasmtime run --wasm component-model --env PWD --env USER --dir=. --dir=/tmp component.wasm arg1 arg2 arg3 arg4 arg5
+
+
+# old commands
+
+# tinygo build -x -target=wasip2 -o main.wasm ./cmd/wasip2-test
+# wasm-tools component embed -w wasi:cli/imports ~/src/bytecodealliance/wasmtime/crates/wasi/wit/ main.wasm -o embedded.wasm
+# wasm-tools component new embedded.wasm -o component.wasm --adapt ./wasi_snapshot_preview1.command.wasm
+# WASMTIME_LOG=trace WASMTIME_BACKTRACE_DETAILS=1 wasmtime run --wasm component-model --env KEY0=VALUE0 --env KEY1=VALUE1 --dir=. --dir=/tmp component.wasm arg1 arg2 arg3 arg4 arg5
