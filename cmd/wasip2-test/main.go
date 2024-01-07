cbc6f6e6a1019e3445c58be99884e004eda6f8fd
Randy Reddig <randy.reddig@fastly.com>
2024-01-08 06:50:57 +1300

    cmd/wasip2-test: WIP

--- cmd/wasip2-test/main.go
+++ cmd/wasip2-test/main.go
@@ -1,8 +1,15 @@
 package main
 
+import "os"
+
 // This requires TinyGo with WASI Preview 2 support
 // https://github.com/dgryski/tinygo/tree/dgryski/wasi-preview-2
 
 func main() {
 	print("hello world\n")
+	for _, arg := range os.Args {
+		print(arg)
+		print(" ")
+	}
+	print("\n")
 }
--- wasip2run.sh
+++ wasip2run.sh
@@ -1,10 +1,13 @@
 # This requires TinyGo with WASI Preview 2 support
 # https://github.com/dgryski/tinygo/tree/dgryski/wasi-preview-2
 
-tinygo build -x -target=wasip2 -o main.wasm ./cmd/wasip2-test
+tinygo build -target=wasip2 -x -o main.wasm ./cmd/wasip2-test
 wasm-tools component embed -w wasi:cli/command $(tinygo env TINYGOROOT)/lib/wasm/wasip2/wit/ main.wasm -o embedded.wasm
 wasm-tools component new embedded.wasm -o component.wasm
 wasmtime run --wasm component-model --env PWD --env USER --dir=. --dir=/tmp component.wasm arg1 arg2 arg3 arg4 arg5
+
+
+# wasmtime serve --wasm component-model component.wasm
 
 
 # old commands
