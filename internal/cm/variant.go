32a3bcc892acf1a78f6cd66c9f40b506f3b229da
Randy Reddig <randy.reddig@fastly.com>
2024-01-12 11:02:28 +1300

    internal/cm: enforce use of Shape[T] in SizedVariant2

--- internal/cm/variant.go
+++ internal/cm/variant.go
@@ -15,30 +15,30 @@ type Variant2[T0, T1 any] interface {
 // SizedVariant2 represents a variant with 2 associated types, at least one of which has a non-zero size.
 // Use UnsizedVariant2 if both T0 or T1 are zero-sized.
 // The memory layout will have additional padding if both T0 and T1 are zero-sized.
-type SizedVariant2[Shape, T0, T1 any] struct {
+type SizedVariant2[S Shape[T0] | Shape[T1], T0, T1 any] struct {
 	disc uint8
 	_    [0]T0
 	_    [0]T1
-	val  Shape
+	val  S
 }
 
-func (v *SizedVariant2[Shape, T0, T1]) Disc() uint {
+func (v *SizedVariant2[S, T0, T1]) Disc() uint {
 	return uint(v.disc)
 }
 
-func (v *SizedVariant2[Shape, T0, T1]) Store0(val T0) {
+func (v *SizedVariant2[S, T0, T1]) Store0(val T0) {
 	store(&v.disc, 0, &v.val, val)
 }
 
-func (v *SizedVariant2[Shape, T0, T1]) Store1(val T1) {
+func (v *SizedVariant2[S, T0, T1]) Store1(val T1) {
 	store(&v.disc, 1, &v.val, val)
 }
 
-func (v *SizedVariant2[Shape, T0, T1]) Load0() (val T0, ok bool) {
+func (v *SizedVariant2[S, T0, T1]) Load0() (val T0, ok bool) {
 	return load[T0](&v.disc, 0, &v.val)
 }
 
-func (v *SizedVariant2[Shape, T0, T1]) Load1() (val T1, ok bool) {
+func (v *SizedVariant2[S, T0, T1]) Load1() (val T1, ok bool) {
 	return load[T1](&v.disc, 0, &v.val)
 }
 
@@ -70,12 +70,12 @@ func (v *UnsizedVariant2[T0, T1]) Load1() (val T1, ok bool) {
 	return load[T1](&v.disc, 0, &v.val)
 }
 
-func store[T any, Shape any, Disc uint8 | uint16 | uint32](disc *Disc, n Disc, ptr *Shape, val T) {
+func store[T any, S any, Disc uint8 | uint16 | uint32](disc *Disc, n Disc, ptr *S, val T) {
 	*(*T)(unsafe.Pointer(ptr)) = val
 	*disc = n
 }
 
-func load[T any, Shape any, Disc uint8 | uint16 | uint32](disc *Disc, n Disc, ptr *Shape) (val T, ok bool) {
+func load[T any, S any, Disc uint8 | uint16 | uint32](disc *Disc, n Disc, ptr *S) (val T, ok bool) {
 	if *disc != n {
 		return val, false
 	}
