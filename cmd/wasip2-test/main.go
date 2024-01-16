1809ccbea46e89e7868cb31513a166450a2c871f
Randy Reddig <randy.reddig@fastly.com>
2024-01-16 11:49:23 -0800

    cmd/wasip2-test: print args again WIP

--- cmd/wasip2-test/main.go
+++ cmd/wasip2-test/main.go
@@ -9,7 +9,15 @@ import (
 // https://github.com/dgryski/tinygo/tree/dgryski/wasi-preview-2
 
 func main() {
+	fmt.Println("Hello world from WebAssembly!")
+	fmt.Print("os.Args: ")
+	for _, arg := range os.Args {
+		fmt.Print(arg, " ")
+	}
+	fmt.Print("\n\n")
+
 	filename := os.Args[1]
+	fmt.Printf("File.Stat() of %s\n", filename)
 	f, err := os.Open(filename)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "os.Open(%q): %v", filename, err)
