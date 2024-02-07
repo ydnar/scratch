a4e8db5c5c97baacd80b45413c65ef7dc168d6a8
Randy Reddig <randy.reddig@fastly.com>
2024-02-07 09:38:57 -0800

    cmd/wasip2-test: use os.Stat directly
    See bug. TODO: fix bug.

--- cmd/wasip2-test/main.go
+++ cmd/wasip2-test/main.go
@@ -25,29 +25,36 @@ func main() {
 	for _, e := range os.Environ() {
 		fmt.Print(e, "\n")
 	}
-	fmt.Print("\n\n")
+	fmt.Print("\n")
 
 	fmt.Print("os.Args: ")
 	for _, arg := range os.Args {
 		fmt.Print(arg, " ")
 	}
-	fmt.Print("\n\n")
+	fmt.Print("\n")
 
 	filename := os.Args[1]
-	fmt.Printf("File.Stat() of %s\n", filename)
+	info, err := os.Stat(filename)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "os.Stat(): %v", err)
+		return
+	}
+	fmt.Printf("os.Stat size: %d\n", info.Size())
+	fmt.Print("\n")
+
 	f, err := os.Open(filename)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "os.Open(%q): %v", filename, err)
 		return
 	}
 	defer f.Close()
-	info, err := f.Stat()
+	info, err = f.Stat()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "File.Stat(): %v", err)
+		fmt.Fprintf(os.Stderr, "(*os.File).Stat(): %v", err)
 		return
 	}
-	fmt.Printf("file info: %#v\n", info)
-	fmt.Print("\n\n")
+	fmt.Printf("(*os.File).Stat size: %d\n", info.Size())
+	fmt.Print("\n")
 
 	var buf [256]byte
 	for {
