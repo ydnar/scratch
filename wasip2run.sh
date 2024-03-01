be1032876eec7b9941332d65c67b297ec560d454
Randy Reddig <randy.reddig@fastly.com>
2024-03-01 15:19:52 -0800

    wasip2run.sh: update to use TinyGo with built-in calls to 'wasm-tools embed' and 'wasm-tools component new'

--- wasip2run.sh
+++ wasip2run.sh
@@ -1,9 +1,9 @@
 # This requires TinyGo with WASI Preview 2 support
 # https://github.com/dgryski/tinygo/tree/dgryski/wasi-preview-2
 
-tinygo build -target=wasip2 -x -o main.wasm ./cmd/wasip2-test
-wasm-tools component embed -w wasi:cli/command $(tinygo env TINYGOROOT)/lib/wasi-cli/wit/ main.wasm -o embedded.wasm
-wasm-tools component new embedded.wasm -o component.wasm
-wasmtime run --wasm component-model --env PWD --env USER --dir=. --dir=/tmp component.wasm ./LICENSE
+tinygo build -target=wasip2 -x -o main.wasm ./cmd/wasip2-test &&
+# wasm-tools component embed -w wasi:cli/command $(tinygo env TINYGOROOT)/lib/wasi-cli/wit/ main.wasm -o embedded.wasm &&
+# wasm-tools component new embedded.wasm -o component.wasm &&
+wasmtime run --wasm component-model --env PWD --env USER --dir=. --dir=/tmp main.wasm ./LICENSE
 
 # wasmtime serve --wasm component-model component.wasm
