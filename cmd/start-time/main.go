0e162535509732fefd96ef8305282092de63950d
Randy Reddig <randy.reddig@fastly.com>
2024-06-10 21:12:35 -0700

    cmd/start-time: improvements

--- cmd/start-time/main.go
+++ cmd/start-time/main.go
@@ -9,7 +9,6 @@ import (
 
 func main() {
 	start := monoTime(runtimeInitTime)
-	// start := time.Unix(runtimeInitTime/1_000_000_000, runtimeInitTime%1_000_000_000)
 	fmt.Printf("runtimeInitTime: %d \n", runtimeInitTime)
 	fmt.Printf("timeStartNano:   %d\n", timeStartNano)
 	fmt.Printf("since init:      %v\n", time.Since(start))
@@ -21,6 +20,10 @@ var runtimeInitTime int64
 //go:linkname timeStartNano time.startNano
 var timeStartNano int64
 
+// monoTime converts a monotonic nanotime to a [time.Time].
+// The resulting Time has a zero-value wall clock,
+// so is only useful for comparing against another Time value
+// to create a Duration.
 func monoTime(mono int64) time.Time {
 	var t time.Time
 	p := (*struct {
@@ -32,11 +35,3 @@ func monoTime(mono int64) time.Time {
 	p.ext = mono - timeStartNano // mirror mono -= startNano in time.Now()
 	return t
 }
-
-func init() {
-	for i := 0; i < 1_000_000; i++ {
-		j += i
-	}
-}
-
-var j int
