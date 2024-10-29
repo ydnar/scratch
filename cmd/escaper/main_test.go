24950359b5d4fa2b483ef51d44f534f4901612ae
Randy Reddig <randy.reddig@fastly.com>
2024-10-28 21:27:13 -0600

    cmd/escaper: workaround for TinyGo/LLVM optimization

--- cmd/escaper/main_test.go
+++ cmd/escaper/main_test.go
@@ -2,11 +2,11 @@ package main
 
 import "testing"
 
+var ok, err *int
+
 func BenchmarkResultInlines(b *testing.B) {
 	r := Result[int, int]{1, 2}
-	var ok, err *int
 	for i := 0; i < b.N; i++ {
 		ok, err = r.Result()
 	}
-	_, _ = ok, err
 }
